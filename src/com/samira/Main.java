ef4e142ee9cb2bb041d4c8f660530555de6c11ba
SamiraKarimi <samira.ka006@gmail.com>
2019-07-18 23:48:41 -0500

    first commit

---
+++ src/com/samira/Main.java
@@ -0,0 +1,29 @@
+package com.samira;
+
+
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+final byte MONTH_IN_YEAR  = 12;
+final byte  PERCENT = 100;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Principle: ");
+        int principle = scanner.nextInt();
+        System.out.print("Annul Interest Rate: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTH_IN_YEAR;
+        System.out.print("Period (Years): ");
+        byte years = scanner.nextByte();
+        int numberOfPayment = years * MONTH_IN_YEAR;
+
+        double mortgage = principle
+                * (monthlyInterest * Math.pow(1 + monthlyInterest,numberOfPayment))
+                /(Math.pow(1 + monthlyInterest, numberOfPayment)-1);
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted );
+    }
+}
